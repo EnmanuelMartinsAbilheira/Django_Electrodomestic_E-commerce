a24708d54717dbdeb9c87f40b6b30642e0d12188
Enmanuel Martins <eel2000martins@hotmail.com>
2024-10-26 00:36:43 +0100

    Product Sales! modifivation in html models and admin

--- django_electrodomestic_ecommerce/store/models.py
+++ django_electrodomestic_ecommerce/store/models.py
@@ -39,7 +39,10 @@ class Product(models.Model):
     marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
     description = models.CharField(max_length=250, default='', blank=True, null=True)
     image = models.ImageField(upload_to='uploads/product/')
-
+    #add sale stuff
+    is_sale = models.BooleanField(default=False)
+    sale_price= models.DecimalField(default=0, decimal_places=2, max_digits=6) #9999,99$
+    
     def __str__(self):
         return self.name 
 
