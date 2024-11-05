ff82e53ad9c7442da9f4c80f11fc0fd832bdd805
Enmanuel Martins <eel2000martins@hotmail.com>
2024-11-05 01:25:05 +0000

    Add Up Cart Totals

--- django_electrodomestic_ecommerce/cart/cart.py
+++ django_electrodomestic_ecommerce/cart/cart.py
@@ -29,6 +29,30 @@ class Cart():
 
         self.session.modified = True
         
+    def cart_total(self):
+        #get product id
+        product_ids = self.cart.keys()
+        #lookup those keys in our products database models
+        products = Product.objects.filter(id__in=product_ids)
+        #get quantities
+        quantities = self.cart
+        #start counting at 0
+        total = 0
+        for key, value in quantities.items():
+            #convert key sting into into so we cando 
+            key = int(key)
+            for product in products:
+                if product.id == key:
+                    if product.is_sale:
+                        total = total + (product.sale_price * value)
+                    else:
+                        total = total + (product.price * value)
+
+
+        return total
+            
+
+
 
     def __len__(self):
         return len(self.cart)
