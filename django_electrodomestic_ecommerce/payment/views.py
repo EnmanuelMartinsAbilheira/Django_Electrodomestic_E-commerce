dc9b7a7f75ef77330da82a11ad2d12480250c82c
Enmanuel Martins <eel2000martins@hotmail.com>
2024-11-14 01:00:21 +0000

    Order Checkout

--- django_electrodomestic_ecommerce/payment/views.py
+++ django_electrodomestic_ecommerce/payment/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
+from cart.cart import Cart
+from payment.form import ShippingForm
+from payment.models import ShippingAddress
 
 # Create your views here.
+
+def checkout(request):
+    #Get the cart
+    cart = Cart(request)
+    cart_products = cart.get_prods
+    quantities = cart.get_quants
+    totals = cart.cart_total()
+
+    
+    if request.user.is_authenticated:
+        #checkput as logged in user 
+        #Shipping user
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        #Shipping form 
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        return render(request, "payment/checkout.html", {"cart_products": cart_products, 'quantities':quantities, 'totals': totals, "shipping_form":shipping_form})
+    else:
+        #
+        shipping_form = ShippingForm(request.POST or None)
+        return render(request, "payment/checkout.html", {"cart_products": cart_products, 'quantities':quantities, 'totals': totals, "shipping_form":shipping_form})
+
+    
+
+
+
 
 def payment_success(request):
 
